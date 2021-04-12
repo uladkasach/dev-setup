9de4a7f317c0257e61a56fb28755134772046ba0
Ulad Kasach <uladkasach@gmail.com>
2021-04-12 06:54:19 -0400

    feat(aliases): backup latest aliases

--- src/bash_aliases.sh
+++ src/bash_aliases.sh
@@ -12,10 +12,19 @@ alias getuuid='uuidgen | tr -d "'"\n"'" | pbcopy && echo "'"  ✔ uuid was copie
 alias ju='npx jest -c jest.unit.config.js'
 alias ji='npx jest -c jest.integration.config.js'
 alias ja='npx jest -c jest.acceptance.config.js'
+alias jal='LOCALLY=true ja'
 
 # aws profiles
 alias use.tugether='export AWS_PROFILE=tugether'
-alias use.ahbode.prod='export AWS_PROFILE=ahbode-prod'
+alias use.ahbode.prod='export AWS_PROFILE=ahbode.prod'
+alias use.whodis.prod='export AWS_PROFILE=whodis.prod'
+alias use.alistokrad.prod='export AWS_PROFILE=alistokrad.prod'
 
 # make sure caps acts like control
 alias caps2control='setxkbmap -option "'"caps:ctrl_modifier"'"'
+
+# make signing into onepass easier
+alias op.signin='eval $(op signin my)'
+
+# make it easier to open the browser
+alias browser='google-chrome'
