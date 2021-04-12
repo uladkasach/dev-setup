4724dc7f1e6f810af715581b2864d12a97aaa661
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2021-04-12 06:53:03 -0400

    feat(install): update install_env.sh - upgrades

--- src/install_env.sh
+++ src/install_env.sh
@@ -5,57 +5,36 @@
 sudo add-apt-repository ppa:deafmute/interception
 sudo apt install interception-tools
 sudo apt install interception-caps2esc
+reboot; # restart the computer to get those in effect
 
 #########################
 ## install chrome
 #########################
-# https://www.google.com/chrome/
-
-#########################zsh
-## install drop box
-#########################
-# https://www.dropbox.com/install-linux
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads;
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb;
+google-chrome; # open it; set it as your defualt
 
 #########################
-## install one password
+## install ssh + generate ssh key for your machine
 #########################
-# https://app-updates.agilebits.com/product_history/CLI
-# extract manually
-sudo mv ~/Downloads/op /usr/local/bin
-# https://support.1password.com/command-line/
+sudo apt-get install ssh -y;
+ssh-keygen; # use the default path to save the key; create your own password
+cat ~/.ssh/id_rsa.pub; # <- view your public key
+# add it to your github account manually
+google-chrome https://github.com/settings/keys
 
-#########################
-## install ssh
-#########################
-sudo apt-get install ssh -y
-# chmod 400 if needed
+########################
+## set git user
+#######################
+git config --global user.email "u...k...@gmail.com" # change me to your email
+git config --global user.name "U... K..." # change me to your name
+git config --global alias.lg "log --pretty=format:'%C(yellow)%h %Cred%ad %C(cyan)%an%Cgreen%d %Creset%s' --date=short"
 
+########################
+## clone this repo
 #########################
-## install vim
-#########################
-sudo apt install vim
-
-# use vim by default
-echo "
-# use vim by default in terminal
-export VISUAL=vim
-export EDITOR=\"$VISUAL\"
-" >> ~/.zshrc
-
-#########################
-## install codium
-#########################
-# per https://vscodium.com/
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
-echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
-sudo apt update && sudo apt install codium
-
-# use microsoft extensions lib
-➜ sudo find / -name product.json
-...
-/usr/share/codium/resources/app/product.json
-
-vim /usr/share/codium/resources/app/product.json
+mkdir -p ~/git/more;
+git clone git@github.com:uladkasach/dev-env-setup.git ~/git/more/dev-env-setup;
 
 #########################
 ## install zsh + oh-my-zsh + spaceship theme
@@ -63,73 +42,91 @@ vim /usr/share/codium/resources/app/product.json
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" && ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-# reset from backup the zsh config
-cp ~/git/more/dev-env-setup/bash_aliases.sh ~/.bash_aliases
+cp ~/git/more/dev-env-setup/src/bash_aliases.sh ~/.bash_aliases # reset from backup the aliases config
+cp ~/git/more/dev-env-setup/src/zshrc.sh ~/.zshrc # reset from backup the zsh config
 
 # make sure we use zsh by default
 echo "
 exec zsh
 " >> ~/.bashrc
 
+# now open a new terminal
+
 # note: if git icon looks weird, make sure to install font that supports it: https://github.com/tonsky/FiraCode :
 sudo apt install fonts-firacode
 
-########################
-## set git user
-#######################
-git config --global user.email "u...k...@gmail.com"
-git config --global user.name "U... K..."
-git config --global alias.lg "log --pretty=format:'%C(yellow)%h %Cred%ad %C(cyan)%an%Cgreen%d %Creset%s' --date=short"
+#########################
+## install vim
+#########################
+sudo apt install vim -y # note: ~/.zshrc already defines that this is default
+
+#########################
+## install codium
+#########################
+# per https://vscodium.com/
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium -y
+
+# use microsoft extensions lib
+# sudo find / -name product.json # reference for finding the full one
+touch ~/.config/VSCodium; 
+echo "
+{
+  \"extensionsGallery\": {
+    \"serviceUrl\": \"https://marketplace.visualstudio.com/_apis/public/gallery\",
+    \"cacheUrl\": \"https://vscode.blob.core.windows.net/gallery/index\",
+    \"itemUrl\": \"https://marketplace.visualstudio.com/items\",
+    \"controlUrl\": \"\",
+    \"recommendationsUrl\": \"\"
+  }
+}
+" >> ~/.config/VSCodium;
 
 #########################
 ## install node + npm + nvm
 #########################
-# install nvm per https://github.com/nvm-sh/nvm and follow latest and greatest instructs
+browser https://github.com/nvm-sh/nvm; # check if newer version avail; update the below version if it is 
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash;
 nvm install lts/erbium # and install v12 lts
 
 #########################
-## show cpu usage in pop os bar
+## install drop box
 #########################
-sudo apt install gnome-shell-extension-appindicator gnome-shell-extension-system-monitor
-# login logout
+google-chrome https://www.dropbox.com/install-linux # see what the latest version is; update the link below if its changed
+wget https://linux.dropbox.com/packages/ubuntu/dropbox_2020.03.04_amd64.deb -P ~/Downloads;
+sudo dpkg -i ~/Downloads/dropbox_2020.03.04_amd64.deb;
+# now start dropbox manually; super + / "dropbox"; alt, try `dropbox start`?
 
 #########################
-## install aliases and profile defaults
+## install one password (https://support.1password.com/command-line-getting-started/)
 #########################
-cp ~/git/more/dev-env-setup/bash_aliases.sh ~/.bash_aliases
+google-chrome https://app-updates.agilebits.com/product_history/CLI # <- check what the latest version is. we have it writen as 1.8.0 atm, but it will probably change by the time you use it. update your commands accordingly
+wget https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_386_v1.8.0.zip -P ~/Downloads;
+unzip ~/Downloads/op_linux_386_v1.8.0.zip -d ~/Downloads/op_linux_386_v1.8.0;
+gpg --receive-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22;
+gpg --verify ~/Downloads/op_linux_386_v1.8.0/op.sig ~/Downloads/op_linux_386_v1.8.0/op;
+sudo mv ~/Downloads/op_linux_386_v1.8.0/op /usr/local/bin;
+op --version;
 
-#########################
-## install docker + docker compose
-#########################
-sudo apt install docker.io
-sudo systemctl enable --now docker
-sudo groupadd docker
-sudo usermod -aG docker $USER # run w/o root attempt 1
-sudo gpasswd -a $USER docker # run w/o root attempt 2
-su - $USER # relogin
-docker --version
-docker run hello-world # check we can run without root
-sudo apt install docker-compose
+# signin
+op signin my.1password.com wendy_appleseed@example.com; # swap with your email
+op.signin; # use this bash alias to signin subsequently; it runs `eval $(op signin my)` for us
+
+# see docs
+google-chrome https://support.1password.com/command-line-getting-started/
+google-chrome https://support.1password.com/command-line/
+# for example, you can backup aws config with `op create document ~/.aws/credentials --title .aws/credentials`
 
 ##########################
 ## install aws cli
 ##########################
-sudo apt-get install awscli
+sudo apt-get install awscli -y;
 
-# restore config into `~/.aws`
-# example:
-# [ahbode.prod]
-# aws_access_key_id = __ID__
-# aws_secret_access_key = __KEY__
-# region=us-east-1
-
-# # this should already be in zsh:
-# # allow sdks to load the ~/.aws config (e.g., node.aws-sdk)
-# echo "
-# # allow aws-sdks to load config (e.g., node.aws-sdk grab region from ~/.aws)
-# export AWS_SDK_LOAD_CONFIG=1
-# " >> ~/.zshrc
+# restore config into '~/.aws`
+mkdir -p ~/.aws
+op get document .aws/config --output ~/.aws/config
+op get document .aws/credentials --output ~/.aws/credentials
 
 # test installation
 use.ahbode.dev # alias was defined by `./bash_aliases`
@@ -149,6 +146,18 @@ ln -s ~/.tfenv/bin/* ~/.local/bin
 # and test install
 which tfenv
 
+#########################
+## install docker + docker compose
+#########################
+sudo apt install docker.io
+sudo systemctl enable --now docker
+sudo groupadd docker
+sudo usermod -aG docker $USER # run w/o root attempt 1
+sudo gpasswd -a $USER docker # run w/o root attempt 2
+su - $USER # relogin
+docker --version
+docker run hello-world # check we can run without root
+sudo apt install docker-compose
 
 #########################
 ## bump max files watched
@@ -161,4 +170,15 @@ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo s
 #########################
 ## vpn client (https://support.system76.com/articles/use-openvpn/)
 #########################
-sudo apt install openvpn network-manager-openvpn-gnome
+sudo apt install openvpn network-manager-openvpn-gnome -y
+# usage e.g.,:
+#  mkdir -p ~/.vpn && op get document .vpn/main.connection.ovpn --output ~/.vpn/main.connection.ovpn
+#  sudo openvpn --config ~/.vpn/main.connection.ovpn
+
+#########################
+## show cpu usage in pop os bar
+#########################
+sudo apt install gnome-shell-extension-appindicator gnome-shell-extension-system-monitor -y
+logout # login logout of DE
+
+
