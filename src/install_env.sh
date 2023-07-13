ca87eca59c2a1435bb16eea695feedd15ee05985
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2023-07-12 23:19:39 -0700

    fix(install): add better description for a command

--- src/install_env.sh
+++ src/install_env.sh
@@ -264,7 +264,7 @@ sudo apt install openvpn network-manager-openvpn-gnome -y
 ########################
 ## add github cli tool; https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
 #######################
-type -p curl >/dev/null || sudo apt install curl -y
+type -p curl >/dev/null || sudo apt install curl -y # install curl if not already installed
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
   && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
   && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
