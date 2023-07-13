fe4c6de72ff435d86704050b73a616e670192355
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2023-07-13 00:26:02 -0700

    fix(terminal): bind interrupt key to ctrl-x

--- src/install_env.sh
+++ src/install_env.sh
@@ -3,8 +3,9 @@
 ## ref: https://askubuntu.com/questions/53688/making-ctrlc-copy-text-in-gnome-terminal
 #########################
 sudo apt install gconf2
-gconftool-2 -t str -s /apps/gnome-terminal/keybindings/copy "<Control>c" && echo 'interrupt is now bound to ctrl+shift+c, now that ctrl+c was bound to copy'
+gconftool-2 -t str -s /apps/gnome-terminal/keybindings/copy "<Control>c"
 gconftool-2 -t str -s /apps/gnome-terminal/keybindings/paste "<Control>v"
+grep -qxF 'stty intr ^X' ~/.profile || echo '\n# bind interrupt key to ctrl-x\stty intr ^X' >> ~/.profile # writes to `~/.profile` if that line is not alrady there; Why add to `~/.profile` specifically?: https://superuser.com/questions/183870/difference-between-bashrc-and-bash-profile/183980#183980
 
 #########################
 ## install vim
